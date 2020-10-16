397ad9710e775a853814e41453480b67c8245f8c
Arne Haak <45362083+arnehaak@users.noreply.github.com>
2020-10-16 22:13:14 +0200

    Improved traffic sign confusion matrix

--- mlvis/confmat.py
+++ mlvis/confmat.py
@@ -1,6 +1,7 @@
 import os
 
 import numpy as np
+import tensorflow as tf
 
 import matplotlib.pyplot as plt
 import matplotlib
@@ -69,25 +70,51 @@ def add_icon_yaxis(ax, pos, icon_img, icon_zoom):
   
 
   
-def plot_traffic_sign_confmat(confmat, xlabel = 'True label', ylabel = 'Predicted label'):
+def plot_traffic_sign_confmat(confmat, normalize_by = 'none', figsize = (12, 8), figdpi = 150, title = 'Confusion matrix', xlabel = 'Predicted label', ylabel = 'True label'):
 
   num_traffic_signs = 21
   icon_zoom = 0.4
+  
+  ##  ###############
+  ##  test_labels = [0,0,0,1,1,0]
+  ##  labels_predicted = [2,2,3,1,0,1]
+  ##  confmat = tf.math.confusion_matrix(test_labels, labels_predicted, num_classes)
+  ##  print(confmat[:5,:5])
+  ##  ##############
+
+  if tf.is_tensor(confmat):
+    confmat = confmat.numpy()
 
   if (len(confmat.shape) != 2) or (confmat.shape[0] != num_traffic_signs) or (confmat.shape[1] != num_traffic_signs):
     raise RuntimeError("Unexpected confusion matrix dimensions!")
+
+   
+  if normalize_by == "none":
+    pass
+  elif normalize_by == "rows":
+    confmat = confmat.astype(np.float)
+    row_sums = confmat.sum(axis = 1)
+    confmat[row_sums > 0, :] = confmat[row_sums > 0, :] / row_sums[row_sums > 0, np.newaxis]
+  elif normalize_by == "cols":
+    confmat = confmat.astype(np.float)
+    col_sums = confmat.sum(axis = 0)
+    confmat[:, col_sums > 0] = confmat[:, col_sums > 0] / col_sums[np.newaxis, col_sums > 0]
+  else:
+    raise RuntimeError("Invalid normalization mode!")
+
     
-  
   traffic_sign_icons = load_traffic_sign_icons(num_traffic_signs)
- 
-  title='Confusion matrix'
-  cmap=plt.cm.Blues
   
+  cmap=plt.cm.Blues
   
   # Make the norm
   norm = matplotlib.colors.Normalize(vmin = np.min(confmat), vmax = np.max(confmat), clip = False)
 
   fig, ax = plt.subplots()
+  
+  fig.set_size_inches(figsize)
+  fig.set_dpi(figdpi)
+  
   ax.imshow(confmat, interpolation='nearest', cmap=cmap)
 
   ax.set_title(title)
@@ -103,8 +130,8 @@ def plot_traffic_sign_confmat(confmat, xlabel = 'True label', ylabel = 'Predicte
   ax.set_yticks(tick_marks)
   ax.set_yticklabels(target_names)
 
-  ax.set_ylabel(ylabel, labelpad = 20)
   ax.set_xlabel(xlabel, labelpad = 10)
+  ax.set_ylabel(ylabel, labelpad = 20)
   
   fig.tight_layout()
 
@@ -119,7 +146,7 @@ def main():
 
   confmat = np.random.rand(21, 21)
     
-  plot_traffic_sign_confmat(confmat, xlabel = 'True label', ylabel = 'Predicted label')
+  plot_traffic_sign_confmat(confmat)
   
   plt.show(block=True)
 
